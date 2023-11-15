251e286bf74063f86df664dbacbbfdeca87cc3d6
tech41-de <mathias.dietrich@tech41.de>
2023-11-15 16:54:28 +0100

    Readme updated

--- AudioCallBackTemplate/AudioCallBackTemplate/DSP.hpp
+++ AudioCallBackTemplate/AudioCallBackTemplate/DSP.hpp
@@ -21,17 +21,22 @@ public:
         micLevel = volume;
     }
     
+    /*
+     Add your DSP code here (no heap allocation, no locks, no file io, no socket calls, no Swift or Objective-C calls.
+     You have only a few milliseconds time depending on buffersize and sample rate.
+     Calculate: seconds = frames / samplerate
+     For example 64 frames at 48000 sample rate gives 0.9583 msec to deliver all samples in this callback
+     */
     void render(float * bufferL, float * bufferR, int frames){
         for (int i=0; i<frames; ++i) {
-            bufferL[i] = bufferL[i] * micLevel;
-            bufferR[i] = bufferL[i];  // Mic is mono on left channel, we send to right channel as well
+            bufferL[i] = bufferL[i] * micLevel; //
+            bufferR[i] = bufferL[i];  // Mic comes in mono on left channel, we copy to right channel as well
         }
     }
     
 private:
     double sr = 0.0;
     double micLevel = 0.8;
-    
 };
 
 #endif /* DSP_hpp */
